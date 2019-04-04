8506a907e0f3fa5d43c18def43e95fd9b200b8c5
Aakash Goel <goelaakash79@gmail.com>
2019-04-04 22:59:45 +0530

    user model added

--- models/User.js
+++ models/User.js
@@ -3,6 +3,13 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     email: String,
     name: String,
+    username: String,
+    imageUrl: String,
+    phone: String,
+    wallet: {
+        spent: Number,
+        budget: Number
+    }
 });
 
 module.exports = User = mongoose.model('user', UserSchema);
