a087dac291cfd3db07b1eff19124426adb61ed67
Aakash Goel <goelaakash79@gmail.com>
2019-04-04 20:44:03 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const cors = require('cors');
+require('dotenv').config();
+require('./config/dbconnection');
+
+app.use(cors());
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/', require('./routes/index'));
+
+app.get('*', (req, res) => {
+    res.send('notfound');
+});
+
+app.listen(process.env.PORT, (err) => {
+    if (err) {
+        console.log("Error in running server");
+        return;
+    }
+    console.log(`Server is up and running on http://localhost:${process.env.PORT}`);
+});
